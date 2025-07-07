4d8bcf78b3d40e3351d9fb30aad3ecf98c37be56
Thy Anh <84632466+thyus10@users.noreply.github.com>
2025-07-07 22:33:20 +0700

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -13,10 +13,10 @@ public class LoginPage {
 
     public LoginPage(Page page) {
         this.page = page;
-        // Use getByPlaceholder for email and password fields, as in Python
+
         this.email = page.getByPlaceholder("Email");
         this.password = page.getByPlaceholder("Password");
-        // Use getByRole for the Login button
+      
         this.loginBtn = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
         this.alertMessage = page.locator("//div[contains(text(),'Email hoặc mật khẩu không đúng !')]");
     }
@@ -38,8 +38,6 @@ public class LoginPage {
     }
 
     public void doLogin(String email, String password) {
-        // If the login form is in a modal, you may need to click a Login button to open it first
-        // page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
         enterEmail(email);
         enterPassword(password);
         clickLogin();
